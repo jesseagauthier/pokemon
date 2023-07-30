6a28c16c3c737555af5de80b79bee7475638078f
jesse gauthier <gaut0486@algonquinlive.com>
2023-07-30 17:52:09 -0400

    reduces code for displaying and catching. event han

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const reloadButton = document.getElementById('reload');
 
 let caughtCount = 0;
 let displayedPokemon = [];
-let pokemonCaught;
+let pokemonCaught = [];
 
 reloadButton.addEventListener('click', reset);
 const loadingElement = document.getElementById('loading');
@@ -16,17 +16,17 @@ caughtCount = pokemonCaught.length;
 numofPokeleft.textContent = caughtCount;
 
 function reset() {
-  displayedPokemon = {}
-  fetchDataAndUpdatePokemon()
+  displayedPokemon = [];
+  fetchDataAndUpdatePokemon();
 }
 
 async function fetchDataAndUpdatePokemon() {
   const updatedPokemonData = await fetchData();
-  let count = Object.keys(displayedPokemon).length;
+  let count = displayedPokemon.length;
   while (count < 20) {
     const randomIndex = Math.floor(Math.random() * updatedPokemonData.length);
     const pokemonData = updatedPokemonData.splice(randomIndex, 1)[0];
-    displayedPokemon[pokemonData.pokemonId] = pokemonData;
+    displayedPokemon.push(pokemonData);
     count++;
   }
   displayPokemons();
@@ -34,21 +34,18 @@ async function fetchDataAndUpdatePokemon() {
 }
 
 async function fetchData() {
-  // console.log('Fetching Pokemon');
   loadingElement.style.display = 'flex';
   const randomOffset = Math.floor(Math.random() * 100);
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=20&offset=${randomOffset}`);
   const { results } = await response.json();
   const fetchedPokemonData = [];
 
-
   for (const { url } of results) {
     const response = await fetch(url);
     const pokemonDataFull = await response.json();
 
     const pokemonMoves = pokemonDataFull.moves.slice(0, 3);
-    const pokemonAbilities = pokemonDataFull.abilities.slice(0, 3)
-    // const disc = pokemonDataFull.flavor_text_entries.indexOf(en)
+    const pokemonAbilities = pokemonDataFull.abilities.slice(0, 3);
 
     const pokemonDataObj = {
       pokemonId: pokemonDataFull.id,
@@ -71,8 +68,7 @@ async function fetchData() {
       }
     }
 
-    const disc = englishDescription;
-    pokemonDataObj.info = disc;
+    pokemonDataObj.info = englishDescription;
     fetchedPokemonData.push(pokemonDataObj);
   }
 
@@ -83,121 +79,64 @@ async function fetchData() {
 function displayPokemons() {
   displayPokemon.innerHTML = '';
 
-  for (const pokemonId in displayedPokemon) {
-    if (displayedPokemon.hasOwnProperty(pokemonId)) {
-      const pokemonData = displayedPokemon[pokemonId];
+  for (const pokemonData of displayedPokemon) {
+    const { pokemonId, pokemonName, pokemonImage } = pokemonData;
 
-      displayPokemon.insertAdjacentHTML(
-        'beforeend',
-        `
+    displayPokemon.insertAdjacentHTML(
+      'beforeend',
+      `
         <div class="pokemoncard uncaught-card" id="pokemon${pokemonId}">
           <div class="pokemoncard__container">
             <div class="pokemoncard__contents">
-              <h3>${pokemonData.pokemonName}</h3>
+              <h3>${pokemonName}</h3>
               <img class="info" id="ability${pokemonId}" data-pokemon-id="${pokemonId}" title="Pokemon Information" src="assets/stats.svg" alt="infobutton">
-              <img class="pokemonimg" src="${pokemonData.pokemonImage}" alt="${pokemonData.pokemonName}" title="${pokemonData.pokemonName}">
+              <img class="pokemonimg" src="${pokemonImage}" alt="${pokemonName}" title="${pokemonName}">
               <div class="ability-list hidden"></div>
             </div>
           </div>
-          <button id="${pokemonId}" data-source="${pokemonId}" data-pokemonName="${pokemonData.pokemonName}" class="pokemoncard__catchbtn">Catch</button>
+          <button id="${pokemonId}" data-source="${pokemonId}" data-pokemonName="${pokemonName}" class="pokemoncard__catchbtn catch">Catch</button>
         </div>
       `
-      );
-      const catchBTN = document.getElementById(`${pokemonId}`);
-      const abilityFlip = document.getElementById(`ability${pokemonId}`);
-      abilityFlip.addEventListener('click', () => displayAbilities(pokemonId));
-      catchBTN.addEventListener('click', () => catchPokemon(catchBTN));
-    }
+    );
+    displayPokemon.addEventListener('click', actions);
   }
 }
 
-function displayAbilities(pokemonId) {
-  const pokemonImage = document.querySelector(`#pokemon${pokemonId} .pokemonimg`);
-  const abilityList = document.querySelector(`#pokemon${pokemonId} .ability-list`);
+// This handles events from each pokemon Card
 
-  if (pokemonImage.style.visibility === 'hidden') {
-    pokemonImage.style.visibility = 'visible';
-    abilityList.classList.add('hidden');
-  } else {
-    pokemonImage.style.visibility = 'hidden';
-    abilityList.classList.remove('hidden');
-
-    abilityList.innerHTML = `
-      <table>
-        <tr>
-          <th>Abilities:</th>
-        </tr>
-        <tr>
-          ${displayedPokemon[pokemonId].pokemonAbilities
-        .map((ability) => `<td>${ability.ability.name}</td>`)
-        .join('')}
-        </tr>
-      </table>
-      <table>
-        <tr>
-          <th>Moves:</th>
-        </tr>
-        <tr>
-          ${displayedPokemon[pokemonId].pokemonMoves
-        .map((move) => `<td>${move.move.name}</td>`)
-        .join('')}
-        </tr>
-      </table>
-    `;
+function actions(e) {
+  // If catch button is clicked
+  if (e.target.classList.contains('catch')) {
+    const pokemonId = e.target.dataset.source;
+    catchPokemon(pokemonId)
+  }
+  // If abilities is clicked
+  if (e.target.classList.contains('info')) { }
+  // If release button is clicked
+  if (e.target.classList.contains('release')) {
+    const pokemonId = e.target.id;
+    releasePokemon(pokemonId);
   }
 }
 
-function displayCaughtAbilities(pokemon) {
-  console.log(pokemon);
-  const pokemonImage = document.querySelector(`#pokemon${pokemon.pokemonId} .pokemonimg`);
-  const abilityList = document.querySelector(`#pokemon${pokemon.pokemonId} .ability-list`);
+function catchPokemon(pokemonId) {
 
-  if (pokemonImage.style.visibility === 'hidden') {
-    pokemonImage.style.visibility = 'visible';
-    abilityList.classList.add('hidden');
-  } else {
-    pokemonImage.style.visibility = 'hidden';
-    abilityList.classList.remove('hidden');
-
-    abilityList.innerHTML = `
-       <p>XP:  ${pokemon.pokemonExperience}</p>
-         <div>
-         <h6>Abilities</h6>
-           <ul>
-               ${pokemon.pokemonAbilities
-        .map((ability) => `<li>${ability.ability.name}</li>`)
-        .join('')}
-           </ul>
-          </div>
-        <div>
-        <h6>Moves</h6>
-          <ul>
-           ${pokemon.pokemonMoves
-        .map((move) => `<li>${move.move.name}</li>`)
-        .join('')}
-          </ul>
-        </div>
-      <p>${pokemon.info}</p>
-    `;
-  }
-}
-
-
-function catchPokemon(clickedElement) {
-  const pokemonId = clickedElement.id;
+  pokemonId = pokemonId;
   if (pokemonCaught.length >= 6) {
     console.log('Ball Limit Reached');
     return;
   }
-  pokemonCaught.push(displayedPokemon[pokemonId]);
-
+  pokemonCaught.push(displayedPokemon.find(pokemon => pokemon.pokemonId === parseInt(pokemonId)));
   caughtCount++;
-  delete displayedPokemon[pokemonId];
+  displayedPokemon = displayedPokemon.filter(pokemon => pokemon.pokemonId !== parseInt(pokemonId));
   numofPokeleft.textContent = caughtCount;
   localStorage.setItem('caughtPokemon', JSON.stringify(pokemonCaught));
-  fetchDataAndUpdatePokemon();
   displayPokemons();
+  displayCaughtPokemons()
 }
+
+
+fetchDataAndUpdatePokemon();
 
 function displayCaughtPokemons() {
   caught = JSON.parse(localStorage.getItem('caughtPokemon'));
@@ -215,22 +154,18 @@ function displayCaughtPokemons() {
               <div class="ability-list hidden"></div>
             </div>
           </div>
-          <button id="caughtpokemon${pokemon.pokemonId}" class="pokemoncard__catchbtn release">Release</button>
+          <button id="${pokemon.pokemonId}" class="pokemoncard__catchbtn release">Release</button>
         </div>
       `
     );
-
-    const releaseButton = document.getElementById(`caughtpokemon${pokemon.pokemonId}`);
-    releaseButton.addEventListener('click', () => releasePokemon(pokemon.pokemonId));
-
-    const abilityFlip = document.getElementById(`ability${pokemon.pokemonId}`);
-    abilityFlip.addEventListener('click', () => displayCaughtAbilities(pokemon));
+    displayCaughtPokemon.addEventListener('click', actions);
   }
 }
 
+
 function releasePokemon(pokemonId) {
   const index = pokemonCaught.findIndex(pokemon => pokemon.pokemonId === pokemonId);
-  if (index !== -1) {
+  if (index !== 0) {
     pokemonCaught.splice(index, 1);
     caughtCount--;
     numofPokeleft.textContent = caughtCount;
@@ -239,4 +174,73 @@ function releasePokemon(pokemonId) {
   }
 }
 
-fetchDataAndUpdatePokemon();
+
+// function displayAbilities(pokemonId) {
+//   const pokemonImage = document.querySelector(`#pokemon${pokemonId} .pokemonimg`);
+//   const abilityList = document.querySelector(`#pokemon${pokemonId} .ability-list`);
+
+//   if (pokemonImage.style.visibility === 'hidden') {
+//     pokemonImage.style.visibility = 'visible';
+//     abilityList.classList.add('hidden');
+//   } else {
+//     pokemonImage.style.visibility = 'hidden';
+//     abilityList.classList.remove('hidden');
+
+//     abilityList.innerHTML = `
+//       <table>
+//         <tr>
+//           <th>Abilities:</th>
+//         </tr>
+//         <tr>
+//           ${displayedPokemon[pokemonId].pokemonAbilities
+//         .map((ability) => `<td>${ability.ability.name}</td>`)
+//         .join('')}
+//         </tr>
+//       </table>
+//       <table>
+//         <tr>
+//           <th>Moves:</th>
+//         </tr>
+//         <tr>
+//           ${displayedPokemon[pokemonId].pokemonMoves
+//         .map((move) => `<td>${move.move.name}</td>`)
+//         .join('')}
+//         </tr>
+//       </table>
+//     `;
+//   }
+// }
+
+// function displayCaughtAbilities(pokemon) {
+//   const pokemonImage = document.querySelector(`#pokemon${pokemon.pokemonId} .pokemonimg`);
+//   const abilityList = document.querySelector(`#pokemon${pokemon.pokemonId} .ability-list`);
+
+//   if (pokemonImage.style.visibility === 'hidden') {
+//     pokemonImage.style.visibility = 'visible';
+//     abilityList.classList.add('hidden');
+//   } else {
+//     pokemonImage.style.visibility = 'hidden';
+//     abilityList.classList.remove('hidden');
+
+//     abilityList.innerHTML = `
+//        <p>XP:  ${pokemon.pokemonExperience}</p>
+//          <div>
+//          <h6>Abilities</h6>
+//            <ul>
+//                ${pokemon.pokemonAbilities
+//         .map((ability) => `<li>${ability.ability.name}</li>`)
+//         .join('')}
+//            </ul>
+//           </div>
+//         <div>
+//         <h6>Moves</h6>
+//           <ul>
+//            ${pokemon.pokemonMoves
+//         .map((move) => `<li>${move.move.name}</li>`)
+//         .join('')}
+//           </ul>
+//         </div>
+//       <p>${pokemon.info}</p>
+//     `;
+//   }
+// }
